7d98cc14a21fc796accd7117aa585014f6aa7235
root <root@Kandukuris.localdomain>
2021-02-04 16:46:23 -0500

    Merge with conflict

--- project1.sh
+++ project1.sh
@@ -1,2 +1,4 @@
 echo "This is the first created project"
 echo "This is the first line edited in the branch"
+echo "This is line in branch for merge conflicts"
+echo "Commit after update in root with out updating branch"
